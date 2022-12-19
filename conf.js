3b9368e745f493db8e9917ecf03ae8bb660c6cc8
Saravanan S <saravanans@boodskap.io>
2022-12-19 17:12:33 +0530

    default conf loaded

--- conf.js
+++ conf.js
@@ -1,1 +1,1 @@
-module.exports = {"web":{"port":4201},"basepath":"","protocol":"http"}
+module.exports = {"web":{"port":4201},"basepath":"","protocol":"http" }
--- webapps/platform-config.js
+++ webapps/platform-config.js
@@ -1,1 +1,1 @@
-var CONFIG={"web":"http://your.domain.name","basepath":"","api":"/api","development":false,"debug":false,"mqtt":{"hostName":"your_host_or_ip","portNo":80,"ssl":false},"googleAnalytics":"","cdnPath":"https://cdn.jsdelivr.net/gh/BoodskapPlatform/cdn"}
+var CONFIG={"web":"http://your.domain.name","basepath":"","api":"/api","development":false,"debug":false,"mqtt":{"hostName":"your_host_or_ip","portNo":80,"ssl":false},"googleAnalytics":"","cdnPath":"https://cdn.jsdelivr.net/gh/BoodskapPlatform/cdn" }
